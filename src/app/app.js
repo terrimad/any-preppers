0944c1a4a894c664485008ca748d1a9f535a9327
theomuller <theomuller@hotmail.com>
2020-03-24 17:11:49 +0100

    implement routing

--- src/app/app.js
+++ src/app/app.js
@@ -2,26 +2,48 @@ import React from 'react';
 import styled from '@emotion/styled';
 import { ThemeProvider } from 'emotion-theming'
 import { Engineering } from './professions';
+import { Link, Switch, Route } from 'wouter';
+import Router from './router';
+
+const theme = {
+  colors: {
+    text: '#A7A7A7',
+    border: '#A7A7A7',
+  },
+};
 
 export default function App () {
-  return <ThemeProvider theme={{
-    colors: {
-      text: '#A7A7A7',
-      border: '#A7A7A7',
-    },
-  }}>
-    <Wrapper>
-      <Header>Crafty</Header>
-      <Engineering />
-    </Wrapper>
-  </ThemeProvider>;
+  return <Router>
+    <ThemeProvider theme={theme}>
+      <Wrapper>
+        <Link href="/">
+          <Header>Crafty</Header>
+        </Link>
+        <Switch>
+          <Route path="/">
+            <SubHeader>Choose a profession</SubHeader>
+          </Route>
+          <Route path="/engineering" component={Engineering} />
+        </Switch>
+      </Wrapper>
+    </ThemeProvider>
+  </Router>;
 };
 
 const Wrapper = styled.div`
   * {
-    color: ${p => p.theme.colors.text };
+      color: ${p => p.theme.colors.text };
     font-family: 'Open Sans';
   }
+`;
+
+const SubHeader = styled.h2`
+  font-size: 40px;
+  font-weight: 300;
+  padding: 20px 0;
+  display: block;
+  text-align: center;
+  letter-spacing: 2px;
 `;
 
 const Header = styled.h1`
@@ -31,4 +53,5 @@ const Header = styled.h1`
   display: block;
   text-align: center;
   letter-spacing: 2px;
+  cursor: pointer;
 `;
